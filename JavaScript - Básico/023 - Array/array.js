127e250d4cd7398194809c771b1f8d4434051808
Wathyson Samuel <wathysonsamuel61@gmail.com>
2024-10-04 18:58:31 -0300

    aula sobre array

---
+++ JavaScript - Básico/023 - Array/array.js
@@ -0,0 +1,12 @@
+// Arrays são indexados por elementos
+// índice         0        1       2        3
+const alunos = ['Luis', 'Maria', 'João', 'Tiago'];
+console.log(alunos[2])
+
+alunos[4] = 'Julia'
+
+console.log(alunos)
+
+alunos.unshift('Ana') // adiciona no inicio
+
+alunos.push('Antonia') // Adiciona no final
